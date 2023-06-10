0d510efde31f9557a7367dd6611d8ae372e2eef3
buithuytien <buithuytien13.13@gmail.com>
2023-06-10 10:16:27 -0700

    add cuisine - restaurant names

---
+++ TxnGroup3.CS5200.Su1.Txn.R
@@ -0,0 +1,230 @@
+##################################################
+# title: "Implement Transactions"
+# author: "Joshi, Arnav"
+# group: "TxnGroup 3"
+# date: "Summer 1 2023"
+##################################################
+
+
+if("RMySQL" %in% rownames(installed.packages()) == FALSE) {
+  install.packages("RMySQL")
+}
+library("RMySQL")
+
+
+#' Connect to MySQL DB
+#' 
+#' @returns Database connection
+connectMySQL <- function()
+{
+  db_name_fh <- "sql9617943"
+  db_user_fh <- "sql9617943"
+  db_host_fh <- "sql9.freemysqlhosting.net"
+  db_pwd_fh <- "h5xlU6cLN8"
+  db_port_fh <- 3306
+  
+  mydb.fh <- dbConnect(RMySQL::MySQL(), user = db_user_fh, password = db_pwd_fh,
+                       dbname = db_name_fh, host = db_host_fh, port = db_port_fh)
+  mydb <- mydb.fh
+  return(mydb)
+}
+
+
+#' Reads all supplied CSVs into a single large CSV
+#' 
+#' @returns Large single CSV with all data
+readAllCSVs <- function()
+{
+  files <- c("https://5200-assignments.s3.us-east-2.amazonaws.com/synthsalestxns-20230609.csv") # c("transactionsFiveEntries.csv") # TODO: change csv file
+  allRestaurantsDf <- do.call(rbind,lapply(files,read.csv))
+  
+  # drop all NAs rows
+  allRestaurantsDf <- allRestaurantsDf[complete.cases(allRestaurantsDf), ]
+  
+  return(allRestaurantsDf)
+}
+
+
+#' Runs the entire transaction
+#' 
+#' @param dbcon Database connection
+#' @param transactionDfs Dataframe of all transactions
+doTransaction <- function(dbcon, transactionDfs)
+{
+  txnFailed = FALSE
+  
+  dbExecute(dbcon, "START TRANSACTION")
+  
+  
+  for (i in 1:nrow(transactionDfs))
+  {
+    row <- transactionDfs[i,]
+    restaurantName <- transactionDfs[i,]$restaurant
+    cname <- transactionDfs[i,]$name
+    cphone <- transactionDfs[i,]$phone
+    
+    txnFailed <- addRestaurantAndCuisine(dbcon, txnFailed, restaurantName)
+    txnFailed <- addCustomer(dbcon, txnFailed, cname, cphone)
+    txnFailed <- addVisit(dbcon, txnFailed, row)
+  }
+  
+  # commit transaction if no failure, otherwise rollback
+  if (txnFailed == TRUE)
+    dbExecute(dbcon, "ROLLBACK")
+  else
+    dbExecute(dbcon, "COMMIT")
+  
+  # return status; TRUE if successful; FALSE if failed
+  return (!txnFailed)
+}
+
+
+#' Adds restaurant and its cuisine to the DB
+#' 
+#' @param dbcon Database connection
+#' @param txnFailed Whether the entire transactions has failed yet
+#' @param restaurantName Name of restaurant to add
+addRestaurantAndCuisine <- function(dbcon, txnFailed, restaurantName)
+{
+  sql <- paste0("SELECT COUNT(*) FROM restaurants WHERE name = ?restaurantName")
+  sql <- sqlInterpolate(dbcon, sql, restaurantName = restaurantName)
+  res <- dbGetQuery(dbcon, sql)
+  res <- res$`COUNT(*)`
+  
+  if (res == 0)
+  {
+    # Code to insert restaurant
+    cuisineName <- getCuisineFromRestaurant(restaurantName)
+    
+    sql <- paste0("SELECT COUNT(*) FROM cuisine WHERE type = ?cuisineName")
+    sql <- sqlInterpolate(dbcon, sql, cuisineName = cuisineName)
+    res <- dbGetQuery(dbcon, sql)
+    res <- res$`COUNT(*)`
+    
+    if (res == 0)
+    {
+      sql <- paste0("INSERT INTO cuisine (type)
+              VALUES
+              (?cuisineName)")
+      sql <- sqlInterpolate(dbcon, sql, cuisineName = cuisineName)
+      rowNum <- dbExecute(dbcon, sql)
+      if (rowNum < 1)
+        txnFailed = TRUE
+    }
+    
+    sql <- paste0("SELECT zid FROM cuisine WHERE type = ?cuisineName")
+    sql <- sqlInterpolate(dbcon, sql, cuisineName = cuisineName)
+    res <- dbGetQuery(dbcon, sql)
+    zid <- res$zid
+    
+    sql <- paste0("INSERT INTO restaurants
+             (name,zid) 
+             VALUES (?restaurantName, ?zid)")
+    sql <- sqlInterpolate(dbcon, sql, restaurantName = restaurantName, zid = zid)
+    rowNum <- dbExecute(dbcon, sql)
+    if (rowNum < 1)
+      txnFailed = TRUE
+  }
+  
+  return(txnFailed)
+}
+
+
+#' Gets the cuisine for a new restaurant
+#' 
+#' @param restaurantName
+getCuisineFromRestaurant <- function(restaurantName)
+{
+  # TODO: 
+  res <- switch(restaurantName,
+                "El Jefe's" = "Mexican",
+                "McDonald's" = "American",
+                "Chinese123" = "Chinese",
+                "American456" = "American"
+                )
+}
+
+
+#' Adds customer to the DB
+#' 
+#' @param dbcon Database connection
+#' @param txnFailed Whether the entire transactions has failed yet
+#' @param cname Name of customer
+#' @param cphone Phone number of customer
+addCustomer <- function(dbcon, txnFailed, cname, cphone)
+{
+  sql <- paste0("SELECT COUNT(*) FROM customers WHERE cname = ?cname AND cphone = ?cphone")
+  sql <- sqlInterpolate(dbcon, sql, cname = cname, cphone = cphone)
+  res <- dbGetQuery(dbcon, sql)
+  res <- res$`COUNT(*)`
+  
+  if (res == 0)
+  {
+    sql <- "INSERT INTO customers (cname, cphone) VALUES (?cname, ?cphone)"
+    sql <- sqlInterpolate(dbcon, sql, cname = cname, cphone = cphone)
+    rowNum <- dbExecute(dbcon, sql)
+    if (rowNum < 1)
+      txnFailed = TRUE
+  }
+  return(txnFailed)
+}
+
+
+#' Adds visits to the DB
+#' 
+#' @param dbcon Database connection
+#' @param txnFailed Whether the entire transactions has failed yet
+#' @param row Full visit row from large dataframe
+addVisit <- function(dbcon, txnFailed, row)
+{
+  sql <- paste0("SELECT cid FROM customers WHERE cname = ?cname AND cphone = ?cphone")
+  sql <- sqlInterpolate(dbcon, sql, cname = row$name, cphone = row$phone)
+  res <- dbGetQuery(dbcon, sql)
+  cid <- res$cid
+  
+  sql <- paste0("SELECT rid FROM restaurants WHERE name = ?restaurantName")
+  sql <- sqlInterpolate(dbcon, sql, restaurantName = row$restaurant)
+  res <- dbGetQuery(dbcon, sql)
+  rid <- res$rid
+  
+  sql <- paste0("INSERT INTO visits (cid, rid, cc, vdate, amount, `num.guests`)
+              VALUES
+              (?cid, ?rid, ?cc, ?date, ?amount, ?guests)")
+  sql <- sqlInterpolate(dbcon, sql, cid = cid, rid = rid, cc = row$cc, date = row$date, amount = row$amount, guests = row$guests)
+  rowNum <- dbExecute(dbcon, sql)
+  if (rowNum < 1)
+    txnFailed = TRUE
+  return(txnFailed)
+}
+
+
+###########################################################################
+
+
+main <- function()
+{
+  mydb <- connectMySQL()
+  
+  sql <- "SELECT * FROM visits"
+  res <- dbGetQuery(mydb, sql)
+  nrow(res)
+  
+  transactionDfs <- readAllCSVs()
+  print(transactionDfs)
+  
+  txns_status <- doTransaction(mydb, transactionDfs)
+  print(txns_status)
+  
+  # check number of visits after transaction
+  res2 <- dbGetQuery(mydb, sql)
+  nrow(res2)
+  
+  
+  status <- dbDisconnect(mydb)
+}
+
+
+###########################################################################
+
+
+main()
